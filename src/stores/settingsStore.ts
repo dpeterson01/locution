65a71d24bd8f78148f640a126d41cba213f93274
Derek Peterson <drp80@outlook.com>
2026-09-07 16:34:59 -0700

    Upgrade transcribe-cpp to 0.2

--- src/stores/settingsStore.ts
+++ src/stores/settingsStore.ts
@@ -171,7 +171,7 @@ const settingUpdaters: {
   ort_accelerator: (value) =>
     commands.changeOrtAcceleratorSetting(value as OrtAcceleratorSetting),
   transcribe_gpu_device: (value) =>
-    commands.changeTranscribeGpuDevice(value as number),
+    commands.changeTranscribeGpuDevice(value as string | null),
   extra_recording_buffer_ms: (value) =>
     commands.changeExtraRecordingBufferSetting(value as number),
   snippets: (value) => commands.updateSnippetsSetting(value as Snippet[]),
